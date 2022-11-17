49a6a3bfb004557c4f0400b36a72834753817fb2
Kefa698 <kefaombongi07@gmail.com>
2022-11-17 18:22:15 +0300

    first commit

---
+++ contracts/DecentralisedStableCoin.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+// Collateral: Exogenous
+// Minting (Stability Mechanism): Decentralized (Algorithmic)
+// Value (Relative Stability): Anchored (Pegged to USD)
+// Collateral Type: Crypto
+
+// ExoDRCCoin... Which I'm going to call ExoDaCCoin... ExoDac?
+
+// Sometimes refered to just as "Crypto Collateralized Stablecoin" or "Decentralized Stablecoin"
+
+pragma solidity ^0.8.7;
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralisedStableCoin is ERC20Burnable, Ownable {
+    constructor() ERC20("DecentralisedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        require(_amount > 0, "amount is less than zero");
+        require(balance >= _amount, "burn amount exceeds balance");
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        require(_to != address(0), "sho;dnt be address zero");
+        require(_amount > 0, "amount is less tham zero");
+        _mint(_to, _amount);
+        return true;
+    }
+}
